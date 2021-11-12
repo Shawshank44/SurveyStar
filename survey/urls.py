c1496f2a6c115d22f8154f0db0529b18e61f0e46
Shawshank44 <shashankkoushik44@gmail.com>
2021-11-12 07:26:35 -0800

    init

--- survey/urls.py
+++ survey/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from surveystar import views
 from surveystar.views import *
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.Customer_view,name='forms')
+    path('',views.Customer_view,name='forms'),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
